9f7b17a7d23375c2e2f742995ae60d0539503009
Widya Ardianto <nartos9090@gmail.com>
2021-10-21 14:11:09 +0700

    table spp

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,32 @@
 <script>
-	import { Button } from 'sveltestrap'
-	// import { dialog } from 'electron'
-	export let name
+	import { Button, Table, FormCheck } from 'sveltestrap'
+
+	let datas = [
+		{
+			nama: 'Wahyudi',
+			nis: '10001',
+			tahun: 2012,
+			bulan: 1,
+			nominal: 150000,
+			dibayar: true,
+		},
+		{
+			nama: 'Wahyudi',
+			nis: '10001',
+			tahun: 2012,
+			bulan: 2,
+			nominal: 150000,
+			dibayar: true,
+		},
+		{
+			nama: 'Wahyudi',
+			nis: '10001',
+			tahun: 2012,
+			bulan: 3,
+			nominal: 150000,
+			dibayar: false,
+		},
+	]
 
 	const openDialog = () => {
 		window.API.helloDialog()
@@ -9,9 +34,39 @@
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Data Pembayaran SPP</h1>
+	<!-- <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p> -->
 	<Button color="warning" on:click={openDialog}>Hello</Button>
+
+	<Table>
+		<thead>
+			<tr>
+				<td>No.</td>
+				<td>Nama</td>
+				<td>NIS</td>
+				<td>Tahun</td>
+				<td>Bulan</td>
+				<td>Nominal</td>
+				<td>Dibayar</td>
+			</tr>
+		</thead>
+
+		<tbody>
+			{#each datas as data, i}
+			<tr>
+				<td>{i + 1}</td>
+				<td>{data.nama}</td>
+				<td>{data.nis}</td>
+				<td>{data.tahun}</td>
+				<td>{data.bulan}</td>
+				<td>{data.nominal}</td>
+				<td class="d-flex justify-content-center">
+					<FormCheck type="checkbox" readonly bind:checked={data.dibayar} class="ml-auto mr-auto" />
+				</td>
+			</tr>
+			{/each}
+		</tbody>
+	</Table>
 </main>
 
 <style>
